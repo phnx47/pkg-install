18e9d5fae0c570c87f2ba25912fc16ffc8d48b77
Sergey Kuznetsov <cmt@phnx47.net>
2020-01-02 03:55:36 +0700

    Add .editorconfig

--- cargo.sh
+++ cargo.sh
@@ -3,6 +3,6 @@
 DIR="$(dirname "$0")"
 
 if cargo "$@"; then
-    [ -d "$DIR/target/debug" ] && cp -r "$DIR/config" "$DIR/target/debug/config"
-    [ -d "$DIR/target/release" ] && cp -r "$DIR/config" "$DIR/target/release/config"
+  [ -d "$DIR/target/debug" ] && cp -r "$DIR/config" "$DIR/target/debug/config"
+  [ -d "$DIR/target/release" ] && cp -r "$DIR/config" "$DIR/target/release/config"
 fi
