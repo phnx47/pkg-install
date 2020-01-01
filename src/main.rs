3ee6b5c7c8ed0e28e6ea42f4aa3b074474a5022f
Sergey Kuznetsov <cmt@phnx47.net>
2020-01-02 02:29:04 +0700

    Convert code for Ubuntu and Arch

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::process::Command;
 
-use arch_pi::package;
+use post_install::package;
 
 fn main() {
     let mut bash_exec = Command::new("bash");
     let apt_install = bash_exec.arg("-c").arg("sudo apt-get install");
-    let packages = package::read_pacman_packages();
+    let packages = package::read_ubuntu_apt_packages();
 
     for value in packages.iter() {
         println!("Package: {} - {}", value.name, value.desc);
--- src/package.rs
+++ src/package.rs
@@ -1,5 +1,4 @@
 use serde::Deserialize;
-
 use std::fs;
 
 #[derive(Deserialize)]
@@ -8,9 +7,10 @@ pub struct Package {
     pub desc: String,
 }
 
-pub fn read_pacman_packages() -> Vec<Package> {
-    let str_json = fs::read_to_string("/tmp/pacman.json")
-        .expect("Something went wrong reading the pacman.json");
+pub fn read_ubuntu_apt_packages() -> Vec<Package> {
+    let file_path = "./packages/ubuntu/apt.json";
+    let str_json =
+        fs::read_to_string(file_path).expect("Something went wrong reading the config file");
 
     let data: Vec<Package> = serde_json::from_str(&str_json).expect("Deserialize Problem");
     data
