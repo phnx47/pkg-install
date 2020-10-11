89ae6feb69adf393743647b380d1f8fa7266fb8e
Serge K <cmt@phnx47.net>
2020-10-11 17:30:46 +0700

    fix: publish.sh

--- publish.sh
+++ publish.sh
@@ -3,5 +3,5 @@
 ./cargo.sh build --release
 
 mkdir target/publish
-cp target/release/arch-post-install target/publish
+cp target/release/pkg-install target/publish
 cp target/release/config.json target/publish
